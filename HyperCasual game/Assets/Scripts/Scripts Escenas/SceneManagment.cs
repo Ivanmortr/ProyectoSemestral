fd18c124952fb44b92a60081ad84058fb2f30a48
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2021-08-26 09:30:44 -0500

    ultimo commit jiji

--- HyperCasual game/Assets/Scripts/Scripts Escenas/SceneManagment.cs
+++ HyperCasual game/Assets/Scripts/Scripts Escenas/SceneManagment.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class SceneManagment : MonoBehaviour
 {
    [SerializeField] private string _sceneName;
+    [SerializeField] private GameObject _loading;
 
+
+    
    public void ChangeToNewScene()
    {
+        _loading.SetActive(true);
+        DOVirtual.DelayedCall(1.0f, GoNextScene);
+        
+   }
+   private void GoNextScene()
+   {
+
         SceneManager.LoadScene(_sceneName);
    }
 
