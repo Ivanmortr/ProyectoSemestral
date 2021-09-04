77425f2636b9fa6e8bfc1c521768695e15239121
MikeLordx <miguelsantiagoews@gmail.com>
2021-09-04 14:07:16 -0500

    Hola

--- HyperCasual game/Assets/Scripts/Health.cs
+++ HyperCasual game/Assets/Scripts/Health.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using DG.Tweening;
-public class Health : MonoBehaviour,IDamageable
+public class Health : MonoBehaviour, IDamageable
 {
     public delegate void _OnDeath(int amountToIncrease);
     public static event _OnDeath OnDeath;
 
     [SerializeField] private int _maxHealth = 100;
     [SerializeField] private int _currentHealth = 100;
+    [SerializeField] private GameObject _deathPanel;
 
     public int GetHealth => _currentHealth;
     private void Start()
@@ -19,11 +20,15 @@ public class Health : MonoBehaviour,IDamageable
         if (_currentHealth > 0) return;
         OnDeath?.Invoke(BasicBullet.AmountPerDeath);
         Destroy(gameObject);
-
     }
     private void OnDestroy()
     {
         DOTween.Kill(transform);
         DOTween.Kill(gameObject);
+        if (!PlayerData.PlayerAlive&&gameObject.name=="Player")
+        {
+            Debug.Log("Unity");
+            _deathPanel?.SetActive(true);
+        }
     }
 }
