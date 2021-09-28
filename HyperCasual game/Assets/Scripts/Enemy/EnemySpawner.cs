ef8d80940d1380d73c2a8452a88aaacb6d642a25
MikeLordx <miguelsantiagoews@gmail.com>
2021-09-27 21:06:44 -0500

    SpawnerCoolDownUpdate + Boss try

--- HyperCasual game/Assets/Scripts/Enemy/EnemySpawner.cs
+++ HyperCasual game/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -8,7 +8,9 @@ public class EnemySpawner : MonoBehaviour
 
 
     [SerializeField] Transform[] _spawns;
+    [SerializeField] Transform[] _bossSpawns;
     [SerializeField] private int _coolDownTime;
+    [SerializeField] private int _bossCoolDownTime;
     private void Awake()
     {
         _enemyFactory = new EnemyFactory(Instantiate(_enemyConfiguration));
@@ -16,6 +18,7 @@ public class EnemySpawner : MonoBehaviour
     private void Start()
     {
         StartCoroutine(CooldownSpawnZombie(_coolDownTime));
+        StartCoroutine(CoolDownBossSpawn(_bossCoolDownTime));
     }
 
     private IEnumerator CooldownSpawnZombie(int cd)
@@ -25,6 +28,16 @@ public class EnemySpawner : MonoBehaviour
         var randomNumber = Random.Range(0,_spawns.Length);
         _enemyFactory.Create("Zombie", _spawns[randomNumber].transform);
         yield return new WaitForSeconds(cd);
+        }
+    }
+    
+    private  IEnumerator CoolDownBossSpawn(int cdBoss)
+    {
+        while (PlayerData.PlayerAlive)
+        {
+            var randomNumber = Random.Range(0, _bossSpawns.Length);
+            _enemyFactory.Create("Zombie", _bossSpawns[randomNumber].transform);
+            yield return new WaitForSeconds(cdBoss);
         }
     }
 }
