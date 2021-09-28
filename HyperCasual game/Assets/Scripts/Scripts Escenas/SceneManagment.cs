ccbd1b31431c1f6ec4b4abb556dcbe3e49aa409a
MikeLordx <miguelsantiagoews@gmail.com>
2021-09-27 20:53:29 -0500

    UI Changes

--- HyperCasual game/Assets/Scripts/Scripts Escenas/SceneManagment.cs
+++ HyperCasual game/Assets/Scripts/Scripts Escenas/SceneManagment.cs
@@ -4,14 +4,14 @@ using DG.Tweening;
 
 public class SceneManagment : MonoBehaviour
 {
-   [SerializeField] private string _sceneName;
-    [SerializeField] private GameObject _loading;
+    [SerializeField] private string _sceneName;
+    [SerializeField] private string _returnToMainScreen;
 
 
     
    public void ChangeToNewScene()
    {
-        _loading.SetActive(true);
+        
         DOVirtual.DelayedCall(1.0f, Restart);
         
    }
@@ -21,4 +21,11 @@ public class SceneManagment : MonoBehaviour
         PlayerData.CurrencyGold = 0;
         SceneManager.LoadScene(_sceneName);
    }
+    
+    public void MainScreen()
+    {
+        PlayerData.PlayerAlive = true;
+        PlayerData.CurrencyGold = 0;
+        SceneManager.LoadScene(_returnToMainScreen);
+    }
 }
