0a6f27464ceadac43203be1dbfb753d5849f1f85
Lufrauwu <85502316+Lufrauwu@users.noreply.github.com>
2021-10-04 17:54:08 -0500

    High score

--- HyperCasual game/Assets/Scripts/UI/TextEnemiesKilled.cs
+++ HyperCasual game/Assets/Scripts/UI/TextEnemiesKilled.cs
@@ -1,10 +1,17 @@
 using TMPro;
 using UnityEngine;
 
+
 public class TextEnemiesKilled : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _textEnemiesKilled;
     private int _enemyKill = 0;
+    [SerializeField] private TextMeshProUGUI Hscore;
+
+    public void Start()
+    {
+        Hscore.text = PlayerPrefs.GetInt("Text-highscore", 0).ToString();
+    }
 
 
     private void OnEnable()
@@ -16,5 +23,13 @@ public class TextEnemiesKilled : MonoBehaviour
     {
         _enemyKill++;
         _textEnemiesKilled.text = _enemyKill.ToString();
+
+        if(_enemyKill> PlayerPrefs.GetInt("Text-highscore", 0))
+        {
+            PlayerPrefs.SetInt("Text-highscore", _enemyKill);
+            Hscore.text = _enemyKill.ToString();
+        }
+
+        
     }
 }
